24d3e54c625b80c8c7c404b25a4c0e398ec0d2f7
tywi6665 <tyletcole@gmail.com>
2019-01-24 13:59:28 -0700

    updating

--- routes/api/index.js
+++ routes/api/index.js
@@ -9,8 +9,10 @@ require("dotenv").config();
 router.get("/shutterstock/:color", function (req, res) {
     
     var api = shutterstock.v2({
-        clientId: keys.shutterstock.clientId,
-        clientSecret: keys.shutterstock.clientSecret
+        clientId: "e0895-f3fff-50725-48c4b-ee406-c57c5",
+        clientSecret: "d1172-b4866-f0046-3446a-d0282-c188f"
+        // clientId: keys.shutterstock.clientId,
+        // clientSecret: keys.shutterstock.clientSecret
     });
 
     api.image.search({ color: req.params.color }, function (err, data) {
