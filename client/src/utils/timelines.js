dc6df8279a87cb4fb5e16ba7acd75e0a7f299524
tywi6665 <tyletcole@gmail.com>
2019-05-26 12:16:29 -0600

    setting up timeline file

--- client/src/utils/timelines.js
+++ client/src/utils/timelines.js
@@ -1,6 +1,6 @@
 import { TimelineMax as Timeline, Power1 } from "gsap";
 
-const getDefaultTimeline = (node, delay) => {
+const getSavedTimeline = (node, delay) => {
     const timeline = new Timeline({ paused: true });
     const content = node.querSelector("");
     const otherContent = node.querSelector("");
@@ -11,4 +11,37 @@ const getDefaultTimeline = (node, delay) => {
         .from(otherContent, 0.15, { autoAlpha: 0, delay: 0.15, ease: Power1.easeIn })
 
     return timeline;
+};
+
+const getMainTimeline = (node, delay) => {
+    const timeline = new Timeline({ paused: true });
+    const stuff = node.querySelector("");
+
+    timeline
+        .from(node, 0 { display: "none", autoAlpha: 0, delay })
+        .staggerFrom(stuff, 0.375, { autoAplha: 0, x: -25, ease: Power1.easeOut }, 0.125);
+
+    return timeline;
+}
+
+export const play = (pathname, node, appears) => {
+    const delay = appears ? 0 : 0.5;
+    let timeline;
+
+    if (pathname === "/" || pathname === "/main") {
+        timeline = getMainTimeline(node, delay);
+    } else {
+        timeline = getSavedTimeline(node, delay);
+    };
+
+    window
+        .loadPromise
+        .then(() => requestAnimationFrame(() => timeline.play()));
+};
+
+export const exit = (node) => {
+    const timeline = new Timeline({ paused: true });
+
+    timeline.to(node, 0.15, { autoAlpha: 0, ease: Power1.easeOut });
+    timeline.play();
 };
