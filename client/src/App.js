d9f9089f9647e5010a03c3cb60d4f962887d874b
tywi6665 <tyletcole@gmail.com>
2019-05-26 11:47:39 -0600

    setting up tranitiongroups in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Transition, TransitionGroup } from "react-transition-group";
 import Intro from "./pages/Intro";
 import Main from "./pages/Main";
 import Saved from "./pages/Saved";
@@ -8,6 +9,20 @@ import './App.css';
 const App = () => (
   <Router>
     <div>
+      {/* <Route render={({ location }) => {
+        const { pathname, key } = location;
+      
+        return (
+          <TransitionGroup component={null}>
+            <Transition
+              key={key}
+              appear={true}
+              onEnter={(node, appears) => play(pathname, node, appears)}
+              onExit={(node, appears) => exit(node, appears)}
+              timeout={{enter: 750, exit: 150}}
+          </TransitionGroup>
+        )
+      }} /> */}
       <Switch>
         <Route exact path="/" component={Main} />
         <Route exact path="/main" component={Main} />
